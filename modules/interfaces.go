58e33c522d1c8e2af10ac31d2328c7993461ba8a
jaumefe <jaumef.escrig96@gmail.com>
2025-11-06 18:13:14 +0100

    modules eliminate from interface definition specific measurements methods

--- modules/interfaces.go
+++ modules/interfaces.go
@@ -1,27 +1,6 @@
 package modules
 
-type TemperatureDecoder interface {
-	TemperatureFromRaw(rawTemperature []byte) float64
-}
-
-type HumidityDecoder interface {
-	HumidityFromRaw(rawHumidity []byte) float64
-}
-
-type PressureDecoder interface {
-	PressureFromRaw(rawPressure []byte) float64
-}
-
-type GasResistanceDecoder interface {
-	GasResistanceFromRaw(rawGasResistance []byte) float64
-}
-
 type SensorDecoder interface {
-	TemperatureDecoder
-	HumidityDecoder
-	PressureDecoder
-	GasResistanceDecoder
-
 	Decode(raw RawData) Measurement
 	SensorName() string
 }
